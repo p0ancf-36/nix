462632bc77ff9361082d7dc82bda0cc14a4ad9ee
p0ancf-36 <bakhmatov.eugene@yandex.ru>
2025-07-29 21:03:26 +0500

    chore: flake file created

---
+++ flake.nix
@@ -0,0 +1,56 @@
+{
+  description = "My system configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nixvim = {
+     url = "github:nix-community/nixvim";
+     inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: let
+    system = "x86_64-linux";
+    homeStateVersion = "25.05";
+    user = "p0ancf-36";
+    hosts = [
+      { hostname = "eugene_laptop"; stateVersion = "25.05"; }
+    ];
+
+    makeSystem = { hostname, stateVersion }: nixpkgs.lib.nixosSystem {
+      system = system;
+      specialArgs = {
+        inherit inputs stateVersion hostname user;
+      };
+
+      modules = [
+        ./hosts/${hostname}/configuration.nix
+      ];
+    };
+
+  in {
+    nixosConfigurations = nixpkgs.lib.foldl' (configs: host:
+      configs // {
+        "${host.hostname}" = makeSystem {
+          inherit (host) hostname stateVersion;
+        };
+      }) {} hosts;
+
+    homeConfigurations.${user} = home-manager.lib.homeManagerConfiguration {
+      pkgs = nixpkgs.legacyPackages.${system};
+      extraSpecialArgs = {
+        inherit inputs homeStateVersion user;
+      };
+
+      modules = [
+        ./home-manager/home.nix
+      ];
+    };
+  };
+}
